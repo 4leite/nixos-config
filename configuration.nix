eed960713ac3312a062b760e0a1ccf88b761552d
4leite <4leite@gmail.com>
2025-10-10 13:48:06 +1300

    update

--- configuration.nix
+++ configuration.nix
@@ -171,6 +171,14 @@
     google-chrome
     mono
     wine
+    openssl
+    gearlever
+    gimp
+    autokey
+    qbittorrent
+    tor
+    tor-browser
+    android-tools
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];  
